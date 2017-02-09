c837d7c1664249603211d2c3107b179484f631ab
OmairXcluesiv <omair@xcluesiv.com>
2017-02-09 17:00:19 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -4,17 +4,19 @@ require 'scraperwiki.php';
 require 'scraperwiki/simple_html_dom.php';
 
 $counter = 0;
+$data_till = 35;
+
 $html = scraperwiki::scrape('https://www.olx.com.pk/cars/');
 $dom = new simple_html_dom();
 $dom->load($html);
 
-while($counter=35){
+for($i=0;$i<=$data_till;$i++){
 $title = array($dom->find("h3.large"));
+echo $title[$i];
 $counter++;
 }
 echo $counter;
-for($i=0;$i<=$counter;$i++){
- echo $title[$i];
+ 
 }
 
 
